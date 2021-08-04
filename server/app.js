31c1af6a8687252dac20112532ce9e741cc28204
Matheus Galantine <matgalantine@hotmail.com>
2021-08-03 23:44:28 -0300

    exercicio 2

--- server/app.js
+++ server/app.js
@@ -5,16 +5,7 @@ const fs = require('fs')
 const PORT = 3000
 const db = {}
 const app = express()
-
-// carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
-// você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
-// dica: 1-4 linhas de código (você deve usar o módulo de filesystem (fs))
-
-// configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???qual-templating-engine???');
-//app.set('views', '???caminho-ate-pasta???');
-// dica: 2 linhas
-
+const CAMINHO = 'server/data/jogadores.json'
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
@@ -26,12 +17,30 @@ app.listen(PORT, () => {
 })
 
 // EXERCÍCIO 2
+// configurar qual templating engine usar. Sugestão: hbs (handlebars)
+//app.set('view engine', '???qual-templating-engine???');
+//app.set('views', '???caminho-ate-pasta???');
+// dica: 2 linhas
+app.set('view engine', 'hbs')
+app.set('views', 'server/views')
+
+// carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
+// você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
+// dica: 1-4 linhas de código (você deve usar o módulo de filesystem (fs))
+fs.readFile(CAMINHO, (err, data) => {
+    if (err) {
+        throw err
+    }
+    Object.assign(db, JSON.parse(data))
+})
+
 // definir rota para página inicial --> renderizar a view index, usando os
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json (~3 linhas)
-
-
+app.get('/', (req, res) => {
+    res.render('index.hbs', db)
+})
 
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
